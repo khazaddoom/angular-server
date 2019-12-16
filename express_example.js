94e1fdad3336e98d0fbd93a41e08db5224268a3d
Ganesh Acharya <ganesh.a@juegostudio.com>
2019-12-16 15:05:13 +0530

    initial files

---
+++ express_example.js
@@ -0,0 +1,22 @@
+var express = require('express');  
+var app = express();  
+
+app.get('/api/fast', (req, res) => {
+    console.log('fast endpoint hit');
+    res.send({response: 'fast'});
+  });
+  
+  app.get('/api/slow', (req, res) => {
+    setTimeout(() => {
+        console.log('slow endpoint hit');
+        res.send({response: 'slow'});
+    }, 5000);
+  });
+
+var server = app.listen(8081, function () {
+
+    var host = server.address().address;  
+    var port = server.address().port;  
+    console.log("Example app listening at http://%s:%s", host, port);
+
+});  
